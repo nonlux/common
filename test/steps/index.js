cb42f5f0d485ddc4366d7c2f69fed2c9f4040c3e
nonlux <nonluxi@gmail.com>
2016-12-27 10:44:26 +0800

    yadda dictionary

--- test/steps/index.js
+++ test/steps/index.js
@@ -1,8 +1,17 @@
-import { localisation } from 'yadda'; // eslint-disable-line import/no-extraneous-dependencies
+import Yadda, { localisation } from 'yadda'; // eslint-disable-line import/no-extraneous-dependencies
+import { should } from 'chai';
+
+should();
 
 const { English } = localisation;
 
+const dictionary = new Yadda.Dictionary()
+        .define('js', /([^\u0000]*)/,
+                (data, cb) => cb(null, JSON.parse(data)))
+        .define('escapeString', /"([^"]*)"/, (data, cb) => cb(null, data));
+
 export default function libraryInstance() {
-  return English.library()
+  const context = {};
+  return English.library(dictionary)
     .when('foo', () => console.log('foo'));
 }
